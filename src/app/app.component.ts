5930fc4e00a37aee9bc8ace29e868143cc2e5ff9
Mike Wunderlich <ff@undefined-solutions.de>
2021-10-20 08:33:10 +0200

    init(Pagedjs) MVP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { Previewer } from 'pagedjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'masterarbeit-cog';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('content') content: ElementRef;
+  @ViewChild('pdf') pdf: ElementRef;
+
+  ngAfterViewInit(): void {
+    const previewer = new Previewer();
+    previewer
+      .preview(
+        this.content.nativeElement.innerHTML,
+        ['/assets/pagedjs.css'],
+        this.pdf.nativeElement
+      )
+      .then(flow => {
+        console.log("preview rendered, total pages", flow.total, { flow });
+      });
+  }
 }
