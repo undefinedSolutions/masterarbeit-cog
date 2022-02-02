1005b7c03aa9806dd7a90d5deeb04be8d5388c1e
Mike Wunderlich <github@undefined-solutions.de>
2022-02-02 20:27:50 +0100

    init(content) Datenverarbeitung und Bereitstellung, Bereitstellung der Datensätze, Nutzung der Datensätze und Benchmark Datenaufarbeitung

--- src/app/content/c4-methodology/c4-methodology.component.ts
+++ src/app/content/c4-methodology/c4-methodology.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import { TablesService } from './../../services/tables/tables.service';
 
 @Component({
   selector: 'app-c4-methodology',
   templateUrl: './c4-methodology.component.html',
   styleUrls: ['./c4-methodology.component.scss']
 })
-export class C4MethodologyComponent {
+export class C4MethodologyComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private tablesService:TablesService) { }
+
+  ngAfterViewInit(): void {
+    this.tablesService.pushTable('Relevante Hardwarespezifikationen für GDAL', 2)
+  }
 }
