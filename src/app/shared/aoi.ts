c1a137537cd0716440e08ab8fb3f4253b07fe1e4
Mike Wunderlich <github@undefined-solutions.de>
2022-02-06 22:13:15 +0100

    fix(content) state 06.02.2022

---
+++ src/app/shared/aoi.ts
@@ -0,0 +1,35 @@
+export const AoiLindenrain = {
+  attributions: "undefined Solutions GmbH",
+  dsm: {
+    min: 0,
+    max: 20
+  },
+  extent: [
+    975839.4237803141586483,
+    6220091.5274126650765538,
+    977070.5237803141353652,
+    6220877.0674126651138067
+  ],
+  ortho: {
+    min: 0,
+    max: 23
+  }
+}
+
+export const AoiStrenzfeld = {
+  attributions: 'Institut für Geoinformation und Vermessung (IGV), <a href="https://gyrocopter.afg.hs-anhalt.de/" rel="noopener" target="_blank">Hochschule Anhalt</a>',
+  dsm: {
+    min: 0,
+    max: 19,
+  },
+  extent: [
+    1299642.3081000000238419,
+    6765838.1484000002965331,
+    1304883.3142999999690801,
+    6769892.0274999998509884
+  ],
+  ortho: {
+    min: 0,
+    max: 21,
+  }
+}
