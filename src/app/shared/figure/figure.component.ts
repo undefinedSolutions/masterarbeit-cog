69e384f414c0804a239e0fb38dcc4e714e6e3f34
undefinedSolutions <github@undefined-solutions.de>
2022-02-25 11:12:59 +0100

    fix(content) 2.1 und 2.2

--- src/app/shared/figure/figure.component.ts
+++ src/app/shared/figure/figure.component.ts
@@ -12,10 +12,19 @@ export class FigureComponent implements OnInit {
   @Input() caption: string;
   @Input() imageStyle: string;
   @Input() source: string;
+  @Input() sourceQuote = {};
 
   constructor(private figuresService:FiguresService) { }
 
   ngOnInit(): void {
     this.figuresService.pushFigure(this.caption, this.id);
+  }
+
+  getQuote(key): string {
+    return this.sourceQuote[key]
+  }
+
+  checkQuote(): boolean {
+    return Object.keys(this.sourceQuote).length !== 0;
   }
 }
